1d1e006928933633d55df3ce6dda9ef93bd6b60d
“zhaohb” <2996442165@qq.com>
2024-07-12 21:59:49 +0800

    配置完毕

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,120 +1,32 @@
 import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 import { viteBundler } from '@vuepress/bundler-vite'
-// import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
   title: "凹凸的个人网站",
-  description: "Just playing around",
+  description: "凹凸的个人网站",
   bundler: viteBundler(),
-  // bundler: webpackBundler(),
   theme: recoTheme({
-    // style: "@vuepress-reco/style-default",
     logo: "/logo.png",
     author: "凹凸",
     authorAvatar: "/head.png",
-    docsRepo: "https://github.com/au-to",
-    lastUpdatedText: "",
-    docsDir: "/docs", //文档在仓库中的目录
-    // docsBranch: "main", //文档所在的git分支
-    // 自动设置分类
+    lastUpdatedText: "最后更新时间",
+    docsDir: "/docs",
     autoSetBlogCategories: true,
-    // 自动设置系列
     autoSetSeries: true,
-    // 定义侧边栏结构
-    series: {
-      "/docs/theme-reco/": [
-        {
-          text: "module one",
-          children: ["home", "theme"],
-        },
-        {
-          text: "module two",
-          children: ["api", "plugin"],
-        },
-      ]
-    },
-    // 导航栏
+    catalogTitle: '目录',
     navbar: [
       { text: "首页", link: "/" },
-      { text: "分类", link: "/categories/reco/1.html" },
-      { text: "标签", link: "/tags/tag1/1.html" },
+      { text: "分类", link: "/categories/javaScript/1.html" },
+      { text: "标签", link: "/tags/javaScript/1.html" },
       { text: "归档", link: "/timeline.html" },
       {
         text: "文档",
         children: [
-          { text: "vuepress-reco", link: "/docs/theme-reco/theme" }
-        ],
-      }
-    ],
-    // 公告
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: "text",
-    //       content: `我是凹凸`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "QQ 群",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1：1037296104</li>
-    //         <li>QQ群2：1061561395</li>
-    //         <li>QQ群3：962687802</li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "GitHub",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "buttongroup",
-    //       children: [
-    //         {
-    //           text: "打赏",
-    //           link: "/docs/others/donate.html",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-    // 评论
-    // commentConfig: {
-    //   type: 'valine',
-    //   options: {
-    //     appId: 'xxx',
-    //     appKey: 'xxx',
-    //     placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     verify: true, // 验证码服务
-    //     notify: true,
-    //     recordIP: true,
-    //     hideComments: true // 隐藏评论
-    //   },
-    // },
-  }),
-  // debug: true,
-});
+          { text: "关于我", link: "/docs/introduce/myself" }
+        ]
+      },
+      { text: "github", link: "https://github.com/au-to" }
+    ]
+  })
+})
--- README.md
+++ README.md
@@ -8,14 +8,10 @@ modules:
 bannerBrand:
   bgImage: '/bg.svg'
   title: 凹凸的个人网站
-  description: 个人生活随感&技术分享
+  description: 技术分享&个人生活随感
   tagline: 流水不争先，争的是滔滔不绝。
   buttons:
     - { text: 关于我, link: '/docs/introduce/myself.md' }
 blog:
-  socialLinks:
-    # - { icon: 'LogoGithub', link: 'https://github.com/au-to' }
 isShowTitleInHome: true
-actionText: About
-actionLink: /views/other/about
 ---
