b52e1cca54bb3ca07456ef73b59da6059ca2dc28
“zhaohb” <2996442165@qq.com>
2024-08-01 21:32:17 +0800

    添加部署文件

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -4,7 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   title: "凹凸的个人网站",
-  description: "凹凸的个人网站",
+  description: "欢迎访问凹凸的个人网站，这里分享最新的软件开发技巧、编程项目和技术文章，帮助您提升开发技能。关注我们，获取实用的实战项目、开发教程及更多技术资源",
   bundler: viteBundler(),
   theme: recoTheme({
     logo: "/logo.png",
@@ -15,6 +15,13 @@ export default defineUserConfig({
     autoSetBlogCategories: true,
     autoSetSeries: true,
     catalogTitle: '目录',
+    commentConfig: {
+      type: 'waline',
+      options: {
+        serverURL: 'https://blog-comments-server-t4qa-al8uz3i65-au-tos-projects.vercel.app/',
+        hideComments: false, // 全局隐藏评论，默认 false
+      },
+    },
     navbar: [
       { text: "首页", link: "/" },
       { text: "分类", link: "/categories/javaScript/1.html" },
